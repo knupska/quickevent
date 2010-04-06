d42936ccdfdc0f5c5f2cf418ddb04c289cd2874f
Nathan Martin <nathan@knupska.com>
2010-04-07 00:08:17 +1000

    Fixed issue with shortcut in 2.0.6

--- assets/shortcut_quickevent.js
+++ assets/shortcut_quickevent.js
@@ -1,10 +1,13 @@
 jQuery(document).ready(function() {
 	
-	Symphony.Language.add({
-	    'Go to QuickEvent' : false
-	});
-	
-	// Add the 'show/hide all' button
-	jQuery('form h2').append('<a class="button" title="Go to QuickEvent" href="' + Symphony.WEBSITE + '/symphony/extension/quickevent/edit/">' + Symphony.Language.get('Go to QuickEvent') + '</a>');
-
+	// Add the QuickEvent shortcut for 2.0.7+
+	if(Symphony.Language.add) {
+		Symphony.Language.add({'Go to QuickEvent':false});
+		jQuery('form h2').append('<a class="button" title="Go to QuickEvent" href="' + Symphony.WEBSITE + '/symphony/extension/quickevent/edit/">' + Symphony.Language.get('Go to QuickEvent') + '</a>');
+		
+	// Add the QuickEvent shortcut for 2.0.6
+	} else {
+		$website = jQuery('script[src]')[0].src.match('(.*)/symphony')[1];
+		jQuery('form h2').append('<a class="button" title="Go to QuickEvent" href="' + $website + '/symphony/extension/quickevent/edit/">' + 'Go to QuickEvent' + '</a>');
+	}
 });
